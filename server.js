18f284ff14970d188c1f5d5269658da3673ad7bb
Manish V <imvmanish19@gmail.com>
2020-06-17 16:13:19 +0530

    basic frontend code added

--- server.js
+++ server.js
@@ -1,8 +1,27 @@
 const express = require('express');
 const app = express();
+const {Center,Season,Course} = require('./model')
 
 app.set('view engine','hbs');
 app.set('views','public')
+
+app.get('/batchcode',async (req,res) => {
+    try {
+        const centers = await Center.findAll();
+        const seasons = await Season.findAll();
+        const courses = await Course.findAll();
+        const years = [2020,2021,2022];
+        res.render('index',{
+            centers,
+            seasons,
+            courses,
+            years
+        })
+    }
+    catch(e) {
+        console.error(e);
+    }
+})
 
 module.exports = {
     app
